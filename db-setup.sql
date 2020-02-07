95f4886c31e3cb63644dd8b6df80e75ec12aa8b7
bayleyjensen <bayley3jensen0@gmail.com>
2020-02-07 15:00:10 -0700

    SQL done/ Setup almost done

--- db-setup.sql
+++ db-setup.sql
@@ -1,4 +1,4 @@
--- USE keepr13;
+-- USE keeprofkeys;
 
 -- CREATE TABLE vaults (
 --     id int NOT NULL AUTO_INCREMENT,
--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
 export const domain = "dev-tq7abyrf.auth0.com";
 export const audience = "https://KeeprApi.com";
-export const clientId = "ql3Fsll2j4rjK9CFhGXJ438e3d8GX5Q5";
+export const clientId = "QUVuCFLdmJ5UWMDVslPVEk48uvu2lp40";
