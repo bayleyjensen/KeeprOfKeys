1b45093403a657319a9f1712ad68cdc4c322ad16
bayleyjensen <bayley3jensen0@gmail.com>
2020-02-07 13:57:31 -0700

    setup

--- wwwsrc/src/authConfig.js
+++ wwwsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "";
-export const audience = "";
-export const clientId = "";
+export const domain = "dev-tq7abyrf.auth0.com";
+export const audience = "https://KeeprApi.com";
+export const clientId = "ql3Fsll2j4rjK9CFhGXJ438e3d8GX5Q5";
